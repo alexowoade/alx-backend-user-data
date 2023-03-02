d5e89a54374fb999c3fae0316ff17ac5571f4d52
Obelem <ibbraidejesse@gmail.com>
2023-03-02 11:30:34 +0100

    removed docstrings to be at 5 lines

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,25 +5,8 @@ import re
 
 def filter_datum(
         fields: list[str], redaction: str,
-        message: str, separator: str
-) -> str:
-    """a basic data filter
-
-    Args:
-        fields (list[str]): list of strings representing
-        all fields to obfuscate
-
-        redaction (str): string representing by what the
-        field will be obfuscated
-
-        message (str): string representing the log line
-
-        separator (str):  string representing by which character is separating
-        all fields in the log line (message)
-
-    Returns:
-        str: the log message obfuscated:
-    """
+        message: str, separator: str) -> str:
+    '''implement data filter'''
     for field in fields:
         message = re.sub(f'{field}=.*?{separator}',
                          f'{field}={redaction}{separator}', message)
