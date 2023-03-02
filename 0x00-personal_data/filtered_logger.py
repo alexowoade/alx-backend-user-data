321fa9c164e868018bf4ca34c1252c7f57341595
Obelem <ibbraidejesse@gmail.com>
2023-03-02 11:43:30 +0100

    removed docstrings to be at 5 lines

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """Personal data project"""
 import re
+from typing import List
 
 
-def filter_datum(fields: list[str], redaction: str,
+def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
-    '''implement data filter'''
-    for field in fields:
-        message = re.sub(f'{field}=.*?{separator}',
-                         f'{field}={redaction}{separator}', message)
+    """ Returns a log message obfuscated """
+    for f in fields:
+        message = re.sub(f'{f}=.*?{separator}',
+                         f'{f}={redaction}{separator}', message)
     return message
