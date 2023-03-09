3cc94f06b6c1a6e75ea2ddd37e1c4a7196d714d7
Obelem <ibbraidejesse@gmail.com>
2023-03-09 08:53:10 +0100

    started first piece of session auth

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Implement Session Authentication"""
+from .auth import Auth
+
+
+class SessionAuth(Auth):
+    '''defines methods and attributes for implementing
+    session authentication'''
+    pass
