652d89fa138b4d82138e31745c572aba70f5fa93
Obelem <ibbraidejesse@gmail.com>
2023-03-09 10:41:43 +0100

    edited require auth ++

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -44,12 +44,12 @@ def filter_request():
     if auth.authorization_header(request) is None:
         abort(401)
 
-    if auth.current_user(request) is None:
-        abort(403)
-
     if auth.authorization_header(request) is None\
             and auth.session_cookie(request) is None:
         abort(401)
+
+    if auth.current_user(request) is None:
+        abort(403)
 
     request.current_user = auth.current_user(request)
 
