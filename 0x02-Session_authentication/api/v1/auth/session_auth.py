203c22a22817193359be64f8932a86b8b1753ec1
Obelem <ibbraidejesse@gmail.com>
2023-03-09 09:38:36 +0100

    create a session and assign user_id to it

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,9 +1,28 @@
 #!/usr/bin/env python3
 """Implement Session Authentication"""
 from .auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     '''defines methods and attributes for implementing
     session authentication'''
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """create a session ID for a user_id
+
+        Args:
+            user_id (str, optional): User id. Defaults to None.
+
+        Returns:
+            str: a uuid for user
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid4())
+
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id
