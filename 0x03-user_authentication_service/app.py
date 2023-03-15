314441dc3728abb5e18c348bb9dbe93565de3857
Obelem <ibbraidejesse@gmail.com>
2023-03-15 17:41:06 +0100

    added status code explicity

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -97,7 +97,7 @@ def get_reset_password_token() -> str:
 
 
 @app.route('/reset_password', methods=['PUT'], strict_slashes=False)
-def update_password() -> str:
+def update_password() -> Tuple[str, int]:
     """update password
 
     Returns:
@@ -111,7 +111,7 @@ def update_password() -> str:
     except Exception:
         abort(403)
 
-    return jsonify({"email": email, "message": "Password updated"})
+    return jsonify({"email": email, "message": "Password updated"}), 200
 
 
 if __name__ == "__main__":
