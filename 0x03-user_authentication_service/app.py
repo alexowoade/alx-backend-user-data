22ef613f349b3fb42ccae9bbe2f16f392b925af1
Obelem <ibbraidejesse@gmail.com>
2023-03-15 16:02:55 +0100

    implemented logout

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 '''basic flask app'''
-from typing import Tuple
-from flask import Flask, jsonify, abort, request
+from typing import Tuple, Union
+from flask import Flask, jsonify, abort, request, redirect
 from auth import Auth
 
 app = Flask(__name__)
@@ -50,6 +50,16 @@ def login() -> str:
     response = jsonify({'email': email, 'message': 'logged in'})
     response.set_cookie('session_id', session_id)
     return response
+
+
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout() -> Union[abort, redirect]:
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if session_id is None or user is None:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect('/')
 
 
 if __name__ == "__main__":
