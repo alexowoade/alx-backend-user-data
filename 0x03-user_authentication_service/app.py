a76a2e33c5fd70825a66fc2d98b6ba649bb05769
Obelem <ibbraidejesse@gmail.com>
2023-03-15 16:37:49 +0100

    implemented profile function

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -70,5 +70,20 @@ def logout() -> Union[abort, redirect]:
     return redirect('/')
 
 
+@app.route('/profile', strict_slashes=False)
+def profile() -> str:
+    """access profile if session id exists
+    Returns:
+        str: user email
+    """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+
+    if session_id is None or user is None:
+        abort(403)
+
+    return jsonify({'email': user.email})
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
