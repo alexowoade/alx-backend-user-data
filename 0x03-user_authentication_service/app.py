7509c05285404d6751802d3919d06dd25cb86504
Obelem <ibbraidejesse@gmail.com>
2023-03-15 13:20:13 +0100

    registered users after auth

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 '''basic flask app'''
+from typing import Tuple
 from flask import Flask, jsonify
+from auth import Auth
 
 app = Flask(__name__)
+
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
@@ -12,6 +16,24 @@ def index() -> str:
         str: JSON payload
     '''
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=[POST], strict_slashes=False)
+def users() -> Tuple[str, int]:
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        new_user = AUTH.register_user(email, password)
+        if new_user is not None:
+            return jsonify({
+                "email": f"{email}",
+                "message": "user created"
+            }), 200
+    except Exception:
+        return jsonify({
+            "message": "email already registered"
+        }), 400
 
 
 if __name__ == "__main__":
