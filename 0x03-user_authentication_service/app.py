4d992002873838450fe697fe6857d72742c39a88
Obelem <ibbraidejesse@gmail.com>
2023-03-15 17:46:19 +0100

    added type annotation

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -97,7 +97,7 @@ def get_reset_password_token() -> str:
 
 
 @app.route('/reset_password', methods=['PUT'], strict_slashes=False)
-def update_password() -> Tuple[str, int]:
+def update_password() -> str:
     """update password
 
     Returns:
