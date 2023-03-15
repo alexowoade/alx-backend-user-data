dcc0caa241faf053e7b39d368e7d19bdb730d8fc
Obelem <ibbraidejesse@gmail.com>
2023-03-15 18:00:42 +0100

    removed execution perms

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -103,20 +103,13 @@ def update_password() -> Tuple[str, int]:
     Returns:
         str: JSON payload containing email and success message
     """
-    try:
-        email = request.form['email']
-        reset_token = request.form['reset_token']
-        new_password = request.form['new_password']
-    except KeyError:
-        abort(400)
-
+    reset_token = request.form.get('reset_token')
+    new_password = request.form.get('new_password')
+    email = request.form.get('email')
     try:
         AUTH.update_password(reset_token, new_password)
     except Exception:
         abort(403)
-
-    if email is None:
-        abort(400)
 
     return jsonify({"email": email, "message": "Password updated"}), 200
 
